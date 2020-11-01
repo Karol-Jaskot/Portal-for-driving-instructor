9999144a9a24a7023688e131ab99a8933097815a
Karol Jaskot <54280023+Karol-Jaskot@users.noreply.github.com>
2020-11-01 21:11:55 +0100

    Working exam

--- src/main/java/pl/jaskot/portalfordrivinginstructor/Frontend/view/TestView.java
+++ src/main/java/pl/jaskot/portalfordrivinginstructor/Frontend/view/TestView.java
@@ -1,6 +1,7 @@
 package pl.jaskot.portalfordrivinginstructor.Frontend.view;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
@@ -16,8 +17,14 @@ public class TestView extends VerticalLayout {
 
     private VerticalLayout examView;
     private QuestionList questionList;
+    private List<String> question;
     private List<List<String>> questions;
+    private RadioButtonGroup<String> radioGroup;
     private Button startExam;
+    private Button nextQuestion;
+    private Label scoreLabel;
+    private String answer;
+    private int score;
 
     private H1 title;
 
@@ -26,19 +33,8 @@ public class TestView extends VerticalLayout {
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         addClassName("test-view");
 
-
-        questionList = new QuestionList();
-        questions = questionList.getQuestions();
-
-        examView = new VerticalLayout();
-        startExam = new Button("Zacznij egzamin", event -> {
-            createExamView();
-        }) ;
-
-        title = new H1("Egzamin");
+        createContent();
         add(title,startExam, examView);
-
-
     }
 
     private void createExamView() {
@@ -46,15 +42,61 @@ public class TestView extends VerticalLayout {
         examView.removeAll();
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         int random = new Random().nextInt(questions.size() - 1);
-        List<String> question = questions.get(random);
+        question = questions.get(random);
 
         H2 questionText = new H2(question.get(1));
 
-        RadioButtonGroup<String> radioGroup = new RadioButtonGroup<>();
+        radioGroup = new RadioButtonGroup<>();
         radioGroup.setItems(question.get(2), question.get(3), question.get(4));
         radioGroup.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
+        radioGroup.addValueChangeListener(event -> {
+            if (event.getValue() != null) {
+                answer= event.getValue();
+            }
+        });
 
-        examView.add(questionText,radioGroup);
+        scoreLabel.setText("Wynik: "+score);
+        examView.add(questionText,radioGroup,scoreLabel, nextQuestion);
+    }
+
+    private void createContent() throws FileNotFoundException {
+        questionList = new QuestionList();
+        questions = questionList.getQuestions();
+        scoreLabel = new Label("Wynik: -");
+
+        nextQuestion = new Button("Następne pytanie", event -> checkQuestion());
+
+        examView = new VerticalLayout();
+        startExam = new Button("Zacznij egzamin", event -> {
+            score = 0;
+            createExamView();
+        }) ;
+
+
+        title = new H1("Egzamin");
+    }
+
+    private void checkQuestion(){
+        boolean index = question.get(5).contains("A");
+        if(index){
+            if(question.get(2) == answer)
+                score++;
+            index = false;
+        }
+        index = question.get(5).contains("B");
+        if(index){
+            if(question.get(3) == answer)
+                score++;
+            index = false;
+        }
+        index = question.get(5).contains("C");
+        if(index){
+           if(question.get(4) == answer)
+                score++;
+            index = false;
+        }
+
+        createExamView();
     }
 
 }
